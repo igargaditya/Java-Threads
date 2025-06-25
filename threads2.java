9fb87fb86b05052e03bc320f1de01ae6f0b197b2
igargaditya <igargaditya@gmail.com>
2025-06-25 19:17:13 +0530

    Day1

---
+++ threads2.java
@@ -0,0 +1,24 @@
+class threads2 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        threads2 t1 = new threads2();
+        System.out.println(t1.getState()); // NEW
+        t1.start();
+        System.out.println(t1.getState()); // RUNNABLE
+        Thread.sleep(100); // Main thread is slept
+        System.out.println(Thread.currentThread().getState());
+        System.out.println(t1.getState()); // TIMED_WAITING
+        t1.join();
+        System.out.println(t1.getState()); // RUNNABLE
+
+    }
+}
