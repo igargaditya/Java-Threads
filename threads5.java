b6cff69e4d49f29c42a6b620cc508e217785984c
igargaditya <igargaditya@gmail.com>
2025-06-26 00:37:26 +0530

    Synchronization

---
+++ threads5.java
@@ -0,0 +1,40 @@
+class Counter {
+    private int count;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class thread extends Thread {
+    private Counter counter;
+
+    public thread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++)
+            counter.increment();
+    }
+}
+
+public class threads5 {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        thread t1 = new thread(counter);
+        thread t2 = new thread(counter);
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.getCount()); // Doesnt give 2000
+    }
+}
