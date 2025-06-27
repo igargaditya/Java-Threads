9ff984b3bf8dd6a24ee0ba90f37cd98006d0a8a1
igargaditya <igargaditya@gmail.com>
2025-06-27 23:53:41 +0530

    Reenterant Locks

--- threads7.java
+++ threads7.java
@@ -1,29 +1,30 @@
+
+// Reentrant locks 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class threads7 {
     private final Lock lock = new ReentrantLock();
 
-    public synchronized void outermethod() {
-        // lock.lock();
-        // try {
-        System.out.println("Outer Method Runs");
-        innermethod();
-        // } finally {
-        // lock.unlock();
-        // }
+    public void outermethod() {
+        lock.lock();
+        try {
+            System.out.println("Outer Method Runs");
+            innermethod();
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void innermethod() {
-        // lock.lock();
+    public void innermethod() {
+        lock.lock();
 
-        // try {
-        System.out.println("Inner Method Runs");
+        try {
+            System.out.println("Inner Method Runs");
 
-        // }
-        // finally {
-        // lock.unlock();
-        // }
+        } finally {
+            lock.unlock();
+        }
     }
 
     public static void main(String[] args) {
